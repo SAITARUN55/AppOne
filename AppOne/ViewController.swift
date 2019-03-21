a9241f88ba5d8696ee7fc95eb81b33b0d820b5c4
Kaniganti Sai Tarun <ravikiran@thirumalareddys-macbook-air.local>
2019-03-21 20:37:19 +0530

    Initial Commit

---
+++ AppOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by Thirumalareddy Ravi Kiran on 21/3/19.
+//  Copyright © 2019 Kaniganti Sai Tarun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
